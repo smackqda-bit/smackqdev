6650446328ed8884065bee8214f03b4394109545
smackqda-bit <smackq.da@gmail.com>
2026-05-30 02:10:27 +0300

    redeploy

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
 
-const repo = "smackqdev"; // <-- ВАЖНО: имя репозитория
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
-
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  
 };
 
 
