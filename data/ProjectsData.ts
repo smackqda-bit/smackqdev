fc8b3d195a708a1d1b20cfe99544a77b812265d7
smackqda-bit <smackq.da@gmail.com>
2026-05-30 01:24:11 +0300

    change_user

--- data/ProjectsData.ts
+++ data/ProjectsData.ts
@@ -8,4 +8,5 @@ export const ProjectsData:ProjectInterface[] = [
         description: "SmackFlow is a modern platform focused on artificial intelligence and neural networks, built for everyone — from designers and developers to marketers, students, and office professionals."
     },
     
+    
 ]
