7e5f08a38cff4cd647c1e62ccd5321bc99589110
smackqda-bit <smackq.da@gmail.com>
2026-06-03 07:20:18 +0300

    change project img

--- app/components/UI/ProjectCard.tsx
+++ app/components/UI/ProjectCard.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image"
+import Link from "next/link"
+import { FiArrowUpRight } from "react-icons/fi";
 import { ProjectInterface } from "@/Interfaces/ProjectsInterface"
 export default function ProjectCard({title, srcImage, description, id}: ProjectInterface ) {
     return (
         <div>
-            <h1 className="text-xl mb-5 text-white">{title}</h1>
+            <Link target="_blank" rel="noopener noreferrer"  href={'https://smackflow.space/'}>
+            <div className="flex gap-3 items-center mb-5">
+                <h1 className="text-xl  text-white">{title}</h1>
+                <FiArrowUpRight size={30} className="text-white/50 hover:text-white transition-colors" />
+            </div>
+            
             
             <div className="relative w-full h-56 sm:h-48 overflow-hidden rounded-2xl">
                 <Image 
@@ -16,6 +23,7 @@ export default function ProjectCard({title, srcImage, description, id}: ProjectI
                 />
             </div>
             <p className="mt-5 text-white/50 line-clamp-3">{description}</p>
+            </Link>
 
         </div>
     )
